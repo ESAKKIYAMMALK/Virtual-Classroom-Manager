9a0fddefb6a58a761f2f18e9d749b699ff5e01e9
Esakkiyammal K <154653348+ESAKKIYAMMALK@users.noreply.github.com>
2025-10-01 21:16:04 +0530

    Create Shape.java

---
+++ Design Patterns/structural/bridge/Shape.java
@@ -0,0 +1,7 @@
+package structural.bridge;
+
+abstract class Shape {
+    protected Renderer renderer;
+    protected Shape(Renderer renderer) { this.renderer = renderer; }
+    public abstract void draw();
+}
