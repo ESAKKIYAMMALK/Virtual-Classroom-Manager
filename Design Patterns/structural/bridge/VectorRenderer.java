2fd5bf75d37f0bfe06ad39fb4f66ddc3a8e85a20
Esakkiyammal K <154653348+ESAKKIYAMMALK@users.noreply.github.com>
2025-10-01 21:17:12 +0530

    Create VectorRenderer.java

---
+++ Design Patterns/structural/bridge/VectorRenderer.java
@@ -0,0 +1,7 @@
+package structural.bridge;
+
+class VectorRenderer implements Renderer {
+    public void renderCircle(int radius) {
+        System.out.println("Drawing vector circle of radius " + radius);
+    }
+}
