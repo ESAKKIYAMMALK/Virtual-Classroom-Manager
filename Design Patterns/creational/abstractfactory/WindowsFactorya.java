78753aadaeb9104c2598517ce0301989acdd9293
Esakkiyammal K <154653348+ESAKKIYAMMALK@users.noreply.github.com>
2025-10-01 20:48:13 +0530

    Delete Design Patterns/creational/abstractfactory/WindowsFactorya.java

--- Design Patterns/creational/abstractfactory/WindowsFactorya.java
+++
@@ -1,6 +0,0 @@
-package creational.abstractfactory;
-
-class WindowsFactory implements GUIFactory {
-    public Button createButton() { return new WindowsButton(); }
-    public Checkbox createCheckbox() { return new WindowsCheckbox(); }
-}
