c0e24f9a372615c71cdafb5122b2a3160191b2ec
Esakkiyammal K <154653348+ESAKKIYAMMALK@users.noreply.github.com>
2025-10-01 21:16:39 +0530

    Create RasterRenderer.java

---
+++ Design Patterns/structural/bridge/RasterRenderer.java
@@ -0,0 +1,7 @@
+package structural.bridge;
+
+class RasterRenderer implements Renderer {
+    public void renderCircle(int radius) {
+        System.out.println("Drawing raster circle of radius " + radius);
+    }
+}
