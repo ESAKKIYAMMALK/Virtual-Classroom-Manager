24ced4900ac6d667998b65b6b52a1b8e9b10952a
Esakkiyammal K <154653348+ESAKKIYAMMALK@users.noreply.github.com>
2025-10-01 20:57:19 +0530

    Update ChainDemo.java

--- Design Patterns/behavioral/chain/ChainDemo.java
+++ Design Patterns/behavioral/chain/ChainDemo.java
@@ -1,42 +1,5 @@
 package behavioral.chain;
 
-interface Handler {
-    void setNext(Handler next);
-    void handle(Request req);
-}
-
-class Request {
-    String type;
-    String payload;
-    Request(String t, String p) { type = t; payload = p; }
-}
-
-class AuthHandler implements Handler {
-    private Handler next;
-    public void setNext(Handler next) { this.next = next; }
-    public void handle(Request req) {
-        if (req.type.equals("AUTH")) System.out.println("AuthHandler handled: " + req.payload);
-        else if (next != null) next.handle(req);
-        else System.out.println("Unhandled: " + req.type);
-    }
-}
-
-class LoggingHandler implements Handler {
-    private Handler next;
-    public void setNext(Handler next) { this.next = next; }
-    public void handle(Request req) {
-        if (req.type.equals("LOG")) System.out.println("LoggingHandler handled: " + req.payload);
-        else if (next != null) next.handle(req);
-        else System.out.println("Unhandled: " + req.type);
-    }
-}
-
-class DefaultHandler implements Handler {
-    private Handler next;
-    public void setNext(Handler next) { this.next = next; }
-    public void handle(Request req) { System.out.println("Default handling: " + req.payload); }
-}
-
 public class ChainDemo {
     public static void main(String[] args) {
         Handler auth = new AuthHandler();
